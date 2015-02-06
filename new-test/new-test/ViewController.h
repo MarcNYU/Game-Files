f6a7b6f0eb440b1771df00d3a1bf308a2d6e37c0
Marcus Williams <maw593@nyu.edu>
2015-02-06 10:32:31 -0500

    test

---
+++ new-test/new-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  new-test
+//
+//  Created by Marcus Williams on 2/5/15.
+//  Copyright (c) 2015 Marcus Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ new-test/new-test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  new-test
+//
+//  Created by Marcus Williams on 2/5/15.
+//  Copyright (c) 2015 Marcus Williams. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
